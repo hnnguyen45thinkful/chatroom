54c63e1fc3ca802bdf702a55b0da815c9a43277e
Hieu Nguyen <hnnguyen45@gmail.com>
2016-12-07 00:11:17 -0800

    added socket.io into main.js, index.html, and server.js

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
+//Starting off the jQuery Code.
 $(document).ready(function() {
+    //add var socket to connect Socket.IO server.
+    var socket = io();
     var input = $('input');
     var messages = $('#messages');
 
@@ -6,13 +9,18 @@ $(document).ready(function() {
         messages.append('<div>' + message + '</div>');
     };
 
-    input.on('keydown', function(event) {
-        if (event.keyCode != 13) {
-            return;
-        }
+input.on('keydown', function(event) {
+    if (event.keyCode != 13) {
+        return;
+    }
 
-        var message = input.val();
-        addMessage(message);
-        input.val('');
-    });
+    var message = input.val();
+    addMessage(message);
+    //Added socket.emitt with message that sends to socket.io server.
+    socket.emit('message', message);
+    input.val('');
+});
+    //Add a listener for the event for the Client.
+    socket.on('message', addMessage);
+
 });
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+//Created and edited by Hieu Nguyen
+//Added the socket_io 
+var socket_io = require('socket.io');
+var http = require('http');
 var express = require('express');
 
 var app = express();
 app.use(express.static('public'));
 
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+
+io.on('connection', function (socket) {
+    console.log('Client connected');
+    //Here you add a new listener to the socket which is used to communicate with the client. When a message with the name message is received on the socket you simply print out the message. Restart the server and try entering a message. You should see it getting printed by the server.
+    socket.on('message', function(message) {
+        console.log('Received message:', message);
+        socket.broadcast.emit('message', message);
+    });
+});
 app.listen(process.env.PORT || 8080, function(){
     console.log('Server is running at http://localhost:8080');
 });
